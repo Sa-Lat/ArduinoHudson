1181885234ba4b75c91cbc9d8e8886d2b39a9ad6
Sascha Latzkowski <s.latzkowski@queo-group.com>
2015-01-21 16:36:27 +0100

    Better color definitions

--- ArduinoHudson.ino
+++ ArduinoHudson.ino
@@ -13,12 +13,12 @@ String location = "/queo/index.php HTTP/1.0";
 
 // if need to change the MAC address (Very Rare)
 
-byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0xD3, 0x5F };
-//byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0x20, 0x78 };
+//byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0xD3, 0x5F };
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0x20, 0x78 };
 ////////////////////////////////////////////////////////////////////////
 
-IPAddress ip(10,0,51,193);
-//IPAddress ip(10,0,51,194);
+//IPAddress ip(10,0,51,193);
+IPAddress ip(10,0,51,194);
 
 #define PIN            6
 #define NUMPIXELS      40
@@ -140,7 +140,7 @@ void checkResponse(String response) {
        pixelRegister ++;
     }
     
-    pixels.setBrightness(15);
+    pixels.setBrightness(30);
     pixels.show();
   }
 }
@@ -151,17 +151,17 @@ uint32_t getColorFromString(String colorString) {
       if (colorString == "red") {
           color = pixels.Color(255,0,0);
       } else if (colorString == "blue") {
-          color = pixels.Color(0,255,0);
+          color = pixels.Color(0,80,0);
       } else if (colorString == "disabled") {
           color = pixels.Color(0,0,255);
       } else if (colorString == "red_anime" || colorString == "aborted_anime") {
-          color = pixels.Color(255,162,0);
+          color = pixels.Color(255,100,0);
       } else if (colorString == "blue_anime") {
           color = pixels.Color(255,255,0);
       } else if (colorString == "aborted") {
         color = pixels.Color(0,255,255);
       } else {
-          color = pixels.Color(10,10,0);
+          color = pixels.Color(255,0,255);
       }
       
       return color;
