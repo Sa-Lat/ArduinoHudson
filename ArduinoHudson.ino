52ac56dd7cd234c4b773f66ed5f025fc01397802
Sascha Latzkowski <s.latzkowski@queo-group.com>
2015-01-28 15:47:24 +0100

    Refactored Code and added Text-Ooutput for failed Projects

--- ArduinoHudson.ino
+++ ArduinoHudson.ino
@@ -1,170 +1,235 @@
 #include <Ethernet.h>
 #include <SPI.h>
+
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
 
-////////////////////////////////////////////////////////////////////////
-//CONFIGURE
-////////////////////////////////////////////////////////////////////////
-byte server[] = { 213,174,47,236 }; //ip Address of the server you will connect to
+#define PIN 6
+#define NUMPIXELS 40
+#define COLS 5
+#define ROWS 8
 
-//The location to go to on the server
-//make sure to keep HTTP/1.0 at the end, this is telling it what type of file it is
+//Matrix Conf
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(5, 8, PIN, NEO_MATRIX_TOP + NEO_MATRIX_RIGHT + NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE, NEO_GRB + NEO_KHZ800);
+const uint32_t textColor = matrix.Color(255, 0, 0);
+
+//Ethernet Conf
+byte server[] = { 213,174,47,236 }; //ip Address of the server you will connect to
 String location = "/queo/index.php HTTP/1.0";
 
-// if need to change the MAC address (Very Rare)
-
+//Check MAC on back of Arduino
 //byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0xD3, 0x5F };
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0x20, 0x78 };
-////////////////////////////////////////////////////////////////////////
 
 //IPAddress ip(10,0,51,193);
 IPAddress ip(10,0,51,194);
 
-#define PIN            6
-#define NUMPIXELS      40
-
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 EthernetClient client;
 
-char inString[255]; // string for incoming serial data
-int stringPos = 0; // string index counter
-boolean startRead = false; // is reading?
-int delayval = 10000;
-String colors[40];
+int projectCount = 0; 
+int failedProjectCount = 0;
+boolean startRead = false;
+int delayval = 30000;
+uint16_t colors[40];
+String failedProjects[40];
+int state = 0;
+boolean showFailedProjects = true;
 
-void setup(){
+void setup() {
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(40);
+  matrix.setTextColor(textColor);
+  matrix.fillScreen(0);
+  
   Ethernet.begin(mac, ip);
   Serial.begin(9600);
-  pixels.begin();
 }
 
-void loop(){
-  readAndShowHudsonData();
-  delay(delayval); //wait 5 seconds before connecting again
+void loop() {
+    doMagic();
+    delay(delayval);
 }
 
-void readAndShowHudsonData() {
-  String responseString = connectAndRead(); //connect to the server and read the output
-  checkResponse(responseString);
+void doMagic() {
+  connectAndRead();
 }
 
-String connectAndRead(){
-  //connect to the server
-
+void connectAndRead(){
   Serial.println("connecting...");
-
-  //port 80 is typical of a www page
   if (client.connect(server, 80)) {
     Serial.println("connected");
     client.print("GET ");
     client.println(location);
     client.println();
-
     //Connected - Read the page
-    return readPage(); //go and read the output
-
-  }else{
-    return "connection failed";
+    readAndShowData();
+  } else {
+    Serial.println("connection failed");
+    matrix.fillScreen(0);
+    matrix.setCursor(0, 0);
+    matrix.print(("!"));
+    matrix.show();
   }
-
 }
 
-String readPage(){
-  //read the page, and capture & return everything between '<' and '>'
-
-  stringPos = 0;
-  memset( &inString, 0, 255); //clear inString memory
-
-  while(true){
-
-    if (client.available()) {
-      char c = client.read();
-
-      if (c == '<' ) { //'<' is our begining character
-        startRead = true; //Ready to start reading the part 
-      }else if(startRead){
-        if(c != '>'){ //'>' is our ending character
-          inString[stringPos] = c;
-          stringPos ++;
-        }else{
-          //got what we need here! We can disconnect now
-          startRead = false;
-          client.stop();
-          client.flush();
-          Serial.println("disconnecting.");
-          return inString;
+int readAndShowData(){
+    //read the page, and capture & return everything between '<' and '>'
+    
+    int projPos = 0;
+    int x = 0;
+    char project[20];
+    projectCount = 0; 
+    failedProjectCount = 0;
+    bool readRedProjects;
+    
+    memset( &project, 0, 20); //clear projectColors memory
+    
+    
+    x = matrix.width();
+    matrix.fillScreen(0);
+    
+    while(true){
+        if (client.available()) {
+            char c = client.read();
+            if (c == '<' ) {
+                startRead = true; //Ready to start reading the part
+            }else if(startRead){
+                if(c != '>'){ //'>' is our ending character
+                    if( c == '+')  {
+                        readRedProjects = true;
+                    } else if (readRedProjects) {
+                        if (c != '#') {
+                            if (c != ',') {
+                                project[projPos] = c;
+                                projPos ++;
+                            } else {
+                                failedProjects[failedProjectCount] = project;
+                                failedProjectCount ++;
+                                memset( &project, 0, 20);
+                                projPos = 0;
+                            }
+                        } else {
+                            readRedProjects = false;
+                        }
+                      } else {
+                          if (c != ',') {
+                            colors[projectCount] = getColorFromId(c);
+                            projectCount ++;
+                          }
+                      }
+                    } else {
+                    //got what we need here! We can disconnect now
+                    startRead = false;
+                    client.stop();
+                    client.flush();
+                    Serial.println("disconnecting.");
+                    printFailedProjects();
+                    return true;
+                }
+            }
         }
-      }
+    }
+}
+
+void printFailedProjects() {
+  int x;
+   
+  if (showFailedProjects == true) {
+    for(int i = 0; i < failedProjectCount; i++) {
+    int totalShifts = - ((failedProjects[i].length() + 3) * COLS);   // Total shifts needed
+                                
+    x = matrix.width();
+    matrix.setCursor(x, 0);
+    matrix.fillScreen(0);
+    
+    Serial.println(failedProjects[i]);
+    
+    for(int j = x; j > totalShifts; j--) {
+        matrix.fillScreen(0);
+        matrix.setCursor(j, 0);
+        matrix.print((failedProjects[i]));
+        matrix.show();
+        delay(100);
     }
   }
-}
-
-void checkResponse(String response) {
-  int red = 0;
-  int green = 0;
-  int blue = 0;
-  int factor = 1;
-  int rest = 0;
-  int pixelRegister = 0;
   
-  int commaIndex = -1;
-  int secondCommaIndex = 0;
-  int projectCount = 0;
-  
-  while(secondCommaIndex >= 0) {
-    projectCount ++;
-    
-    secondCommaIndex = commaIndex + 1;
-    secondCommaIndex = response.indexOf(',',secondCommaIndex);
-    String color = response.substring(commaIndex + 1, secondCommaIndex);
-    commaIndex = secondCommaIndex;
-    
-    colors[projectCount - 1] = color;
+    showFailedProjects = false;
+  } else {
+    showFailedProjects = true;
   }
   
+  drawProjectsOnMatrix();
+}
+
+void drawProjectsOnMatrix() {
+  int factor = 1;
+  int rest = 0;
+  int x = 0;
+  int y = 0;
+  
   factor = NUMPIXELS / projectCount;
-  rest = NUMPIXELS - projectCount * factor; 
+  rest = NUMPIXELS - projectCount * factor;
+
+  
+   matrix.fillScreen(0);
+   matrix.setCursor(0, 0);
   
   for(int i = 0; i < projectCount; i++) {
-    String colorString = colors[i];
     int actualFactor = factor;
     
     if (rest > 0) {
        actualFactor ++;
        rest --;
      }
-    
+     
     for (int j = 0; j <= actualFactor -1; j++) {
-       uint32_t color = getColorFromString(colorString);
-       pixels.setPixelColor(pixelRegister, color);
-       pixelRegister ++;
+       
+       matrix.drawPixel(x,y,colors[i]);
+       
+       if (y == ROWS -1) {
+         y = 0;
+         x ++;
+       } else {
+         y ++;
+       }
     }
     
-    pixels.setBrightness(30);
-    pixels.show();
+    matrix.show();
   }
 }
 
-uint32_t getColorFromString(String colorString) {
+uint32_t getColorFromId(char colorId) {
+  
       uint32_t color; 
       
-      if (colorString == "red") {
-          color = pixels.Color(255,0,0);
-      } else if (colorString == "blue") {
-          color = pixels.Color(0,80,0);
-      } else if (colorString == "disabled") {
-          color = pixels.Color(0,0,255);
-      } else if (colorString == "red_anime" || colorString == "aborted_anime") {
-          color = pixels.Color(255,100,0);
-      } else if (colorString == "blue_anime") {
-          color = pixels.Color(255,255,0);
-      } else if (colorString == "aborted") {
-        color = pixels.Color(0,255,255);
+      //red 
+      if (colorId == '1') {
+          color = matrix.Color(255,0,0);
+      //blue
+      } else if (colorId == '2') {
+          color = matrix.Color(0,80,0);
+      //disabled
+      } else if (colorId == '3') {
+          color = matrix.Color(0,0,255);
+      //red_anime, aborted_anime
+      } else if (colorId == '4' || colorId == '5') {
+          color = matrix.Color(255,165,0);
+      //blue_anime
+      } else if (colorId == '6') {
+          color = matrix.Color(255,255,0);
+      //aborted
+      } else if (colorId == '7') {
+        color = matrix.Color(0,255,255);
+      //else
       } else {
-          color = pixels.Color(255,0,255);
+          color = matrix.Color(150,0,150);
       }
       
       return color;
 }
 
- 
+
+
